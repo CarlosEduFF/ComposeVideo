68451c80547f93c71ae2227034077e28ed7fbd98
CarlosEduFF <Carlos.farias11@etec.sp.gov.br>
2023-05-25 09:13:31 -0300

    Compose with Video

---
+++ app/src/main/java/com/example/composetutorial/SampleData.kt
@@ -0,0 +1,11 @@
+package com.example.composetutorial
+
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Sukuna",
+            "Test...Test...Test..."
+        )
+    )
+}
